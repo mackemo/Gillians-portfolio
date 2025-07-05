7c52dfbd34715c4f41586fb2447064c2f1521b29
mackemo <mackenzielmoore14@gmail.com>
2025-07-05 17:29:58 -0500

    Changed layouts and colors last min

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import headshot from '../assets/images/headshot.png';
-import resume from '../assets/images/resume.png';
 import { motion } from 'framer-motion';
 
-function About({ setHideNavbar }) {
-  const [isZoomed, setIsZoomed] = useState(false);
-
-  const handleZoom = () => setIsZoomed(true);
-  const closeZoom = () => setIsZoomed(false);
-
-  // Hide navbar when zoomed in
-  useEffect(() => {
-    if (setHideNavbar) {
-      setHideNavbar(isZoomed);
-    }
-
-    return () => {
-      if (setHideNavbar) setHideNavbar(false);
-    };
-  }, [isZoomed, setHideNavbar]);
-
+function About() {
   return (
     <motion.div
             initial={{ opacity: 0 }}
@@ -27,45 +10,43 @@ function About({ setHideNavbar }) {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
             >
-        
+
         <section>
             <div className="about-container">
-                <div className="stats-container">
-                <div className="stats-header">
-                    <h3>About Me</h3>
-                    <div className="stats">
-                    <p>Gillian Flatt (she/they)</p>
-                    <p>Height: 5'5</p>
-                    <p>Hair Color: Ginger</p>
-                    <p>Eye Color: Green</p>
-                    <p>Location: Indianapolis, IN</p>
+                <h3 className="about-title">About Me</h3>
+
+                <div className="about-content">
+                    <div className="about-left">
+                        <div className="photo-container">
+                            <img className="about-photo" src={headshot} alt="Gillians headshot" />
+                        </div>
+                        <div className="stats">
+                            <p>Gillian Flatt (she/they)</p>
+                            <p>Height: 5'5</p>
+                            <p>Hair Color: Ginger</p>
+                            <p>Eye Color: Green</p>
+                            <p>Location: Indianapolis, IN</p>
+                        </div>
+                    </div>
+
+                    <div className="intro-container">
+                        <p className='intro'>
+                            Murmurs from the house, a last minute prop check, and...
+                            <br></br>
+                            <br></br>
+                            My name is Gillian Flatt and I am an Indianapolis based actor, singer, and costume designer. Throughout the past four years I have 
+                            been cultivating my love for storytelling and pushing myself to learn new tools in order to tell those stories. I hold immense value 
+                            in trying to find new angles and aspects of theatre as we all lift each other up as artists. Outside of theatre I enjoy writing, 
+                            drawing, and showing off pictures of my french bulldog, Kevin! Please feel free to take some time to peruse my past acting and 
+                            costume design projects, and I hope to work with you soon ◡̈
+                            <br></br>
+                            <br></br>
+                            ...Lights down, curtain up, and away we go!
+                        </p>
                     </div>
                 </div>
-                </div>
-
-                <div className="images-row">
-                <div className="resume-container">
-                    <img
-                    className="resume"
-                    src={resume}
-                    alt="Gillians Resume"
-                    onClick={handleZoom}
-                    style={{ cursor: 'zoom-in' }}
-                    />
-                </div>
-                <div className="photo-container">
-                    <img className="about-photo" src={headshot} alt="Gillians headshot" />
-                </div>
-                </div>
             </div>
-
-            {isZoomed && (
-                <div className="modal-overlay" onClick={closeZoom}>
-                <img className="zoomed-resume" src={resume} alt="Zoomed Resume" />
-                </div>
-            )}
-        </section>
-            
+        </section> 
     </motion.div>
   );
 }
